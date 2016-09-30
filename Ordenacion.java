51cc505e2cb764fc950c145a6459344ea6e6bfb8
Alejandra <inzunza251094@gmail.com>
2016-09-29 19:44:33 -0700

    Clase de ordenacion

---
+++ Ordenacion.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen1;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Ordenacion {
+//    int[] ordenar(int[] elements);
+//
+//    public void imprimirArreglo();
+
+    public void ordenar(int[] array) {
+        
+    }
+
+        public void imprimirArreglo(int[] array) {
+
+	}
+    }
+
